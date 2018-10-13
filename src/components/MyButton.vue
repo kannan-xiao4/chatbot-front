4734476eee71f0e231379ba6688cc77cea2df1fb
TakashiKannan <wingrakhi@live.jp>
2018-10-14 00:19:57 +0900

    https://qiita.com/nrslib/items/be90cc19fa3122266fd7 を参考にcomponentを作成

---
+++ src/components/MyButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="onClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+    import {Component, Emit, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class MyButton extends Vue {
+        @Prop()
+        public greet?: string;
+        private count: number = 0;
+
+        @Emit()
+        public clicked(count: number ) {
+        }
+
+        public onClick() {
+            // alert(this.greet);
+            this.count++;
+            this.clicked(this.count);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
+    <p>{{greetText}}</p>
+    <p>greeting count : {{count}} times</p>
+    <p v-if="isRegulars">Thanks for many times</p>
+    <p>
+        <MyButton :greet="greetText" @clicked="onMyButtonClick">hogehoge</MyButton>
+    </p>
+    <p>
+        <ResetButton v-model="greetText"></ResetButton>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {Component, Vue, Watch} from 'vue-property-decorator';
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import MyButton from '@/components/MyButton.vue';
+    import ResetButton from '@/components/ResetButton.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+      components: {
+        ResetButton,
+        MyButton,
+      },
+    })
+
+    export default class Home extends Vue {
+        public greetText: string = 'Hello';
+        private count: number = 0;
+
+        public get isRegulars(): boolean {
+            return this.count >= 5;
+        }
+
+        @Watch('count')
+        public countChanged() {
+            if (this.count === 5) {
+                alert('you are regulars');
+            }
+        }
+
+        public onMyButtonClick(count: number) {
+            this.count = count;
+            this.greetText = 'Hello World!';
+        }
+    }
 </script>
