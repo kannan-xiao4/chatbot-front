e5b60cc3560cb879edc3268c0a89a34e698b294d
TakashiKannan <wingrakhi@live.jp>
2018-10-14 09:30:17 +0900

    firebaseの初期化処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+    apiKey: process.env.FIRE_BASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
